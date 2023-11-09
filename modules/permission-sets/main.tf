5a29faa23f7a4aaaba08edddfe8856cb8cc11544
Ivan Martinez <ivan.martinez@recharge.com>
2023-11-09 15:44:46 +0100

    Fixed the key name used to access the Permission Set in the aws_ssoadmin_customer_managed_policy_attachment resource (#5)

--- modules/permission-sets/main.tf
+++ modules/permission-sets/main.tf
@@ -54,7 +54,7 @@ resource "aws_ssoadmin_customer_managed_policy_attachment" "default" {
     for customer_managed_policy_attachment in local.customer_managed_policy_attachments : "${customer_managed_policy_attachment.name}-${customer_managed_policy_attachment.customer_managed_policy_attachment.path}${customer_managed_policy_attachment.customer_managed_policy_attachment.name}" => customer_managed_policy_attachment
   }
   instance_arn       = local.instance_arn
-  permission_set_arn = aws_ssoadmin_permission_set.default[each.value.policy_set].arn
+  permission_set_arn = aws_ssoadmin_permission_set.default[each.value.name].arn
 
   customer_managed_policy_reference {
     name = each.value.customer_managed_policy_attachment.name
